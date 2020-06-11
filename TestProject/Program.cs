fc3b0931ba9c8ad81794c05d8578a9c06216429c
김동빈 <dongbin300@gmail.com>
2020-06-11 14:49:58 +0900

    initial commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using SimplePerformanceChecker;
+using System;
+using System.Collections.Generic;
+
+namespace TestProject
+{
+    class Program
+    {
+        private static List<int> numbers = new List<int>(){ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        static void Main(string[] args)
+        {
+            PerformanceChecker checker = new PerformanceChecker(Method1, Method2);
+            checker.CountOfPerform = 1_000_000;
+
+            Console.WriteLine(checker.PerformResult());
+            // ================================
+            // Method1 : 1.160833sec
+            // Method2 : 1.874759sec
+            // ================================
+        }
+
+        /// <summary>
+        /// Foreach Method
+        /// </summary>
+        static void Method1()
+        {
+            int fifteen;
+
+            foreach(int number in numbers)
+            {
+                if (number.Equals(15))
+                {
+                    fifteen = number;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Linq Method
+        /// </summary>
+        static void Method2()
+        {
+            int fifteen;
+
+            fifteen = numbers.Find(num => num.Equals(15));
+        }
+    }
+}
